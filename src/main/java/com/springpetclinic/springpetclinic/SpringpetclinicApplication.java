dec861f885f96322f2294d85e79b71afd41458d2
Queeniva <ngulefacbenazea@gmail.com>
2022-09-21 13:00:19 +0100

    initial commits

---
+++ src/main/java/com/springpetclinic/springpetclinic/SpringpetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.springpetclinic.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringpetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springpetclinic/springpetclinic/SpringpetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpetclinic.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
